27acff5a6e6c682310451c16b0380c2b758c06c4
Ben Yeung <benjaminyeung@berkeley.edu>
2021-08-05 17:33:26 -0700

    Working on Nike.com recognization and api call to stockx

--- background.js
+++ background.js
@@ -3,47 +3,23 @@ chrome.runtime.onInstalled.addListener(() => {
         // Stores variables in local storage
         // Manifest v3 may cut background.js after a certain amount of  time
         // To access: chrome.storage.local.get('name', data => {});
-        name: "Ben"
+        styleId: ''
     });
 });
 
 console.log("BACKGROUND script start")
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+function updateStyles(tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
-        chrome.scripting.insertCSS({ //injecting css
-                target: {
-                    tabId: tabId
-                },
-                files: ["./styles/foreground_styles.css"]
-            })
+        let style_id = ''
 
-            .then(() => {
-                console.log("INJECTED FOREGROUND STYLES")
-                chrome.scripting.executeScript({ //injecting js
-                        target: {
-                            tabId: tabId
-                        },
-                        files: ["./foreground.js"]
+        if (tab.url.match('(http|https):\/\/www.nike.com\/t\/.*')) {
+            console.log('NIKE PAGE FOUND');
+            style_id = tab.url.split(/([^\/]*)$/)[1].substring(0, 10);
+            console.log(style_id)
+        }
 
-                    })
-                    .then(() => {
-                        console.log("INJECTED FOREGROUND SCRIPT")
-
-                        //tabs lets you send message from backend to a specific tab
-                        chrome.tabs.sendMessage(tabId, {
-                            message: "change_name",
-                            payload: "John"
-                        })
-                    })
-            }).catch(err => console.log(err))
-    }
-});
-
-//runtime from background sends to foreground, popup, or options (whichever one cathces first)
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === "get_name") {
-        chrome.storage.local.get('name', data => {
+        chrome.storage.local.get('styles', data => {
             if (chrome.runtime.lastError) {
                 sendResponse({
                     message: "fail"
@@ -56,22 +32,116 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             });
         });
 
-        return true; // needed for asynchronous functions to let chrome know to keep line open
-    } else if (request.message === "change_name") {
-        console.log("change name received on backend")
         chrome.storage.local.set({
-            name: request.payload
+            styles: style_id
         }, () => {
             if (chrome.runtime.lastError) {
+                console.log("ERROR OCCURRED")
+                return;
+            }
+            console.log('successfully saved new style id')
+        });
+
+        return true;
+
+    }
+}
+chrome.tabs.onUpdated.addListener(updateStyles);
+chrome.tabs.onActivated.addListener(updateStyles);
+
+//runtime from background sends to foreground, popup, or options (whichever one cathces first)
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    // if (request.message === "get_name") {
+    //     chrome.storage.local.get('name', data => {
+    //         if (chrome.runtime.lastError) {
+    //             sendResponse({
+    //                 message: "fail"
+    //             });
+    //             return;
+    //         }
+    //         sendResponse({
+    //             message: "success",
+    //             payload: data.name
+    //         });
+    //     });
+
+    //     return true; // needed for asynchronous functions to let chrome know to keep line open
+    // } else if (request.message === "change_name") {
+    //     console.log("change name received on backend");
+    //     chrome.storage.local.set({
+    //         name: request.payload
+    //     }, () => {
+    //         if (chrome.runtime.lastError) {
+    //             sendResponse({
+    //                 message: "fail"
+    //             });
+    //             return;
+    //         }
+
+    //         sendResponse({
+    //             message: "success"
+    //         });
+    //     });
+
+    //     return true;
+    // } 
+    if (request.message === "get_product") {
+        console.log("get_product received on backend");
+        chrome.storage.local.get('style_id', data => {
+            if (chrome.runtime.lastError) {
+                console.log("ERROR OCCURRED GETTING STYLE_ID")
+                return;
+            }
+            let api_call = `https://stockx.com/api/browse?_search=${data.style_id}`;
+            console.log(api_call);
+
+            fetch(api_call).then(function (res) {
+                if (res.status !== 200) {
+                    console.log("Error")
+                    sendResponse({
+                        message: "fail"
+                    });
+                    return
+                }
+                console.log("Success")
+
+                res.json().then(function (dataStockX) {
+                    console.log(dataStockX)
+
+                    if (dataStockX.Products.length == 0) {
+                        console.log("Product array empty")
+                        sendResponse({
+                            message: "failed"
+                        })
+                        return;
+                    }
+
+                    let stockxID = dataStockX.Products[0].styleId;
+                    console.log(`STOCKX STYLE ID RETURNED: ${stockxID}`)
+                    console.log(`LOCAL ID RETURNED: ${data.style_id}`)
+
+                    if (!stockxID.includes(data.style_id)) {
+                        sendResponse({
+                            message: "failed"
+                        })
+                        return;
+                    }
+
+                    let productTitle = dataStockX.Products[0].title
+                    console.log(dataStockX.Products[0].title)
+                    sendResponse({
+                        message: "success",
+                        payload: {
+                            title: productTitle
+                        }
+                    })
+                })
+
+            }).catch(function (err) {
                 sendResponse({
                     message: "fail"
                 });
-                return;
-            }
-
-            sendResponse({
-                message: "success"
-            });
+            })
         });
 
         return true;
--- scripts/popup_script.js
+++ scripts/popup_script.js
@@ -1,8 +1,20 @@
 console.log("POPUP script start");
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === "get_style_id") {
+        //document.querySelector('div').innerHTML = `Style ID: ${request.payload}`
+        console.log("Message received in POPUP script")
+        return; // Even with no response to sendResponse() you still need to return
+    }
+});
+
 chrome.runtime.sendMessage({
-    message: "get_name"
+    message: "get_product"
 }, response => {
     if (response.message === 'success') {
-        document.querySelector('div').innerHTML = `Hello ${response.payload}`
+        document.querySelector('div').innerHTML = `${response.payload.title}`
+    } else {
+        document.querySelector('div').innerHTML = `Could not find the product on StockX`
     }
 })
+
